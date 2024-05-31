fc695471440be994f389e075bf54878afe612f47
sauravkr111 <skumarit111@gmail.com>
2024-05-31 12:18:54 +0530

    Create 02-Virtual-Network-Subnet.tf

---
+++ 02-Virtual-Network-Subnet.tf
@@ -0,0 +1,26 @@
+# Define the provider and configure the Azure environment
+provider "azurerm" {
+  features {}
+}
+
+# Define a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+# Define a virtual network
+resource "azurerm_virtual_network" "example_vnet" {
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+# Define a subnet
+resource "azurerm_subnet" "example_subnet" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
